237c7edcb2058f440d8e61ae831a9a558a4b9cb1
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2020-12-18 00:35:50 -0500

    steps towards responsive?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,22 +4,37 @@ let pink = document.getElementById("pink-chunk");
 let orange = document.getElementById("orange-chunk");
 let ftext = document.getElementById("foot-text");
 
+// responsive nav (NOT animated below 1024px)
+let below1024 = window.matchMedia("(max-width: 1024px)");
 
+function responsiveNav(below1024) {
+  if (below1024.matches) {
+    nav.classList.add("spin-back");
+    logo.classList.add("logo-spin-back");
+    pink.classList.add("pink-straighten");
+    orange.classList.add("orange-straighten");
+    ftext.classList.remove("hideMe");
+  }
+}
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
-  navFunction();
-  footFunction();
+  if (below1024.matches) {
+    return false;
+  } else {
+    navFunction();
+    footFunction();
+  }
 };
 
 
 // Get the offset position of the navbar
 let tilty = nav.offsetHeight;
 
-console.log(tilty)
+console.log(tilty);
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navFunction() {
-  if (window.pageYOffset >= tilty) {
+  if (window.pageYOffset >= (tilty - 90)) {
     nav.classList.add("spin-back");
     logo.classList.add("logo-spin-back");
   } else {
@@ -39,3 +54,6 @@ function footFunction() {
     ftext.classList.add("hideMe");
   }
 };
+
+responsiveNav(below1024);
+below1024.addListener(responsiveNav)
